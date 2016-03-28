05e8002f06742ae6a11c1b553be3099daa6bc5fd
Marcus Bernales <mboperator@gmail.com>
2016-03-27 18:17:00 -0700

    iOS is building

--- src/index.ios.js
+++ src/index.ios.js
@@ -35,7 +35,7 @@ class TodoApp extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to the TODO APP
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
