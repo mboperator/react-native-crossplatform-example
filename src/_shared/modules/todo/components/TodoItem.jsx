0d8a9eef27d73b4b7eb38bd192dfd994005788de
Marcus Bernales <mboperator@gmail.com>
2016-04-09 15:27:40 -0700

    More styling

--- src/_shared/modules/todo/components/TodoItem.jsx
+++ src/_shared/modules/todo/components/TodoItem.jsx
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 const { number, string, bool, object } = PropTypes;
-import { ListItem, Heading, Button } from 'grommet';
+import { ListItem, Heading, Button, Box, CheckBox } from 'grommet';
 
-const TodoItem = ({ description, actions, index }) => {
+const TodoItem = ({ description, actions, index, checked }) => {
   return (
     <ListItem
       align="center"
@@ -11,14 +11,17 @@ const TodoItem = ({ description, actions, index }) => {
       pad={{ horizontal: 'medium', vertical: 'small' }}
       separator="bottom"
     >
-      <Heading tag="h4">
-        {description}
-      </Heading>
-      <aside>
+      <Box justify="left" direction="row">
+        <CheckBox checked={checked} />
+        <Heading tag="h4">
+          {description}
+        </Heading>
+      </Box>
+      <Box>
         <Button onClick={() => actions.destroyTodo(index)}>
           Delete Todo
         </Button>
-      </aside>
+      </Box>
     </ListItem>
   );
 };
