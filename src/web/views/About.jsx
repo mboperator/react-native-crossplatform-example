4a646ad0060a24ca7bd5f20e12362129f8b7b5ed
Marcus Bernales <mboperator@gmail.com>
2016-05-11 19:50:58 -0700

    Got user list now

--- src/web/views/About.jsx
+++ src/web/views/About.jsx
@@ -1,16 +1,20 @@
 import React, { PropTypes } from 'react';
 import { connectModule } from 'redux-modules';
 import userModule from 'modules/user';
+import { Tiles, Tile, Header, Spinning } from 'grommet';
 
-const { shape, array, func } = PropTypes;
+const { shape, array, func, bool } = PropTypes;
 
-@connectModule(state => state, userModule)
+const selector = state => state.users.toJS();
+
+@connectModule(selector, userModule)
 export default class About extends React.Component {
   static propTypes = {
     users: shape({
       actions: shape({
         fetch: func,
       }),
+      _loading: bool,
       collection: array,
     }),
   }
@@ -20,10 +24,25 @@ export default class About extends React.Component {
   }
 
   render() {
+    const { collection = [], _loading } = this.props.users;
     return (
-      <div>
-        Users go here :)
-      </div>
+      <Tiles
+        flush={false}
+        justify="center"
+        colorIndex="light-2"
+        full="horizontal"
+      >
+
+        {_loading && <Header>Loading...</Header>}
+        {collection.map(user =>
+          <Tile key={user.id} colorIndex="light-1">
+            <Header>
+              {user.name}
+            </Header>
+          </Tile>
+        )}
+
+      </Tiles>
     );
   }
 }
