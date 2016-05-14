a7c4b32079e44a80783564f25477c80dc8c4187c
Marcus Bernales <mboperator@gmail.com>
2016-05-13 21:52:37 -0700

    Updated gitignore, added android files

---
+++ src/android/index.js
@@ -0,0 +1,8 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('AndroidProject', () => App);
