5b1fdbf26279381b2494d1c8bd6d1221c2161eb4
Marcus Bernales <mboperator@gmail.com>
2016-05-28 15:15:07 -0700

    Example api

--- src/ios/components/TodoList/index.js
+++ src/ios/components/TodoList/index.js
@@ -1,2 +1,9 @@
-import TodoList from './TodoList';
-export default TodoList;
+import TodoListWeb from './TodoListWeb';
+import TodoListAndroid from './TodoListAndroid';
+import TodoListIOS from './TodoListiIOS';
+import platformSpecific from '../decorators/platformSpecific';
+export default platformSpecific({
+  web: TodoListWeb,
+  android: TodoListAndroid,
+  ios: TodoListIOS,
+});
