a4181052f3c44b0933a389825bb0bae0ca85dc26
Marcus Bernales <mboperator@gmail.com>
2016-10-27 07:56:58 -0700

    Made main layout a little nicer

--- src/mobile/components/TodoList/TodoList.js
+++ src/mobile/components/TodoList/TodoList.js
@@ -1,48 +1,21 @@
-import React, {
-  View,
-  Component,
-  ListView,
-  TouchableHighlight,
-  Text,
-} from 'react-native';
-
-const TodoItem = actions => ({description, index}) => (
-  <TouchableHighlight onPress={actions.removeTodo.bind(null, {index: 0})}>
-    <View>
-      <Text>
-        {description}
-      </Text>
-      <View>
-      </View>
-    </View>
-  </TouchableHighlight>
-);
+import React, { Component, PropTypes } from 'react';
+import { List, ListItem, Text } from 'native-base';
 
 export default class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-    });
-
-    this.state = {
-      dataSource: ds.cloneWithRows(this.props.items),
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const dataSource = this.state.dataSource.cloneWithRows(nextProps.items);
-    this.setState({ dataSource });
-  }
+  static propTypes = {
+    collection: PropTypes.array,
+  };
 
   render() {
+    const { collection } = this.props;
     return (
-      <View>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={TodoItem({ removeTodo: this.props.removeTodo })}
-        />
-      </View>
+      <List>
+        {collection.map(object => (
+          <ListItem>
+            <Text>{object.description}</Text>
+          </ListItem>
+        ))}
+      </List>
     );
   }
 }
--- src/mobile/views/Home/Home.js
+++ src/mobile/views/Home/Home.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  TextInput,
-  MapView,
-} from 'react-native';
+import { TextInput, MapView } from 'react-native';
+import { Container, Content, Header, Title } from 'native-base';
 import { connectModule } from 'redux-modules';
-import { Map } from 'immutable';
 
 import locationModule from '../../../_shared/modules/location';
+import TodoList from '../../components/TodoList';
 import styles from './style';
 
 @connectModule(locationModule)
@@ -25,28 +21,28 @@ export default class App extends Component {
       region,
     } = this.props || {};
     return (
-      <View style={styles.container}>
-        <TextInput
-          style={{ height: 45, borderColor: 'gray', borderWidth: 1 }}
-          onSubmitEditing={e => {
-            actions.create({ description: e.nativeEvent.text });
-          }}
-          keyboardType="default"
-        />
-        <Text style={styles.welcome}>
-          {locationModule.name}
-        </Text>
-        <Text style={styles.welcome}>
-          {JSON.stringify(collection)}
-        </Text>
-        <Text style={styles.welcome}>
-          Map
-        </Text>
-        <MapView
-          style={styles.map}
-          region={region}
-        />
-      </View>
+      <Container>
+        <Header>
+          <Title>
+            spotnotes
+          </Title>
+        </Header>
+
+        <Content>
+          <TextInput
+            style={{ height: 45, borderColor: 'gray', borderWidth: 1 }}
+            onSubmitEditing={e => {
+              actions.create({ description: e.nativeEvent.text });
+            }}
+            keyboardType="default"
+          />
+          <TodoList collection={collection} />
+          <MapView
+            style={styles.map}
+            region={region}
+          />
+        </Content>
+      </Container>
     );
   }
 }
